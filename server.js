08607889456cd236a3c9e8485f70ca96b308eb16
Enes YILMAZ <enisyilmaz12@gmail.com>
2017-02-02 20:20:17 +0300

    Adding DELETE /users/login

--- server.js
+++ server.js
@@ -148,16 +148,27 @@ app.post('/users', function(req, res) {
 
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
-		if(token){
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();	
-		}
-	}, function() {
-		res.status(401).send();
+		userInstance = user;
+
+		return db.token.create({
+			token: token
+		});	
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function(e) {
+		res.status(401).send(e);
+	});
+});
+
+app.delete('/users/login',middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function() {
+		res.status(204).send();
+	}).catch(function() {
+		res.status(500).send();
 	});
 });
 
